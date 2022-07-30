9dfec6e83856b099821b43647392b1531aeb552d
miancho <miancho@gmail.com>
2022-07-30 11:04:23 -0300

    vue-tag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -2,8 +2,8 @@ import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
-    config: {
+    property: {
       id: 'GA_MEASUREMENT_ID'
     },
-  }, nuxtApp.$router)
+  })
 })
